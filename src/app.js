d2093edc5d5584cbd4cafb78a509742a0199a0e1
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 00:51:11 +0530

    Create Server

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app=express();
+app.use('/Home',(req,res)=>{
+    res.send("kapil kumar is here and u also know me ");
+})
+app.use('/test',(req,res)=>{
+    res.send("test is runing...");
+})
+app.listen(4000,()=>{
+    console.log("Server is runing sucessful......");
+});
