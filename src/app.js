6e911304e21ea7405208bc8446deaa6be0b9b449
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 01:21:47 +0530

    delete some line of code

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ app.use('/Home',(req,res)=>{
 app.use('/test',(req,res)=>{
     res.send("test is runing...");
 })
-app.use('/k',(req,res)=>{
-    res.send("kkkkk....");
-})
+
 app.listen(4000,()=>{
     console.log("Server is runing sucessful......");
 });
