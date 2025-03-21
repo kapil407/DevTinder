bf29721072782256db557789589b258ae91b6549
kapil keer <kapilkeer1506@gmail.com>
2025-03-21 22:20:57 +0530

    multi route

--- src/app.js
+++ src/app.js
@@ -1,22 +1,29 @@
 import express from "express";
 const app=express();
-
+// in express , firstly request comes and match the route from top to bottom and send response 
 app.use('/user',(req,res,next)=>{
     console.log("first Handler!!");
     next();
-
-},
-(req,res,next)=>{
-    console.log("second Handler!!");
+});
+app.get('/user',(req,res,next)=>{
+    console.log("1 Handler");
     next();
-    // res.send("second Handler is here!!"); //  getting error 
-},
-(req,res,next)=>{
-    console.log("third Handler");
-    next(); // if next Handler is available then go to next handler otherwise go to the next line ;
-    res.send("second Handler is here!!");
-}
-)
+    res.send("first handler");
+});
+
+
+// },
+// (req,res,next)=>{
+//     console.log("second Handler!!");
+//     next();
+//     // res.send("second Handler is here!!"); //  getting error 
+// },
+// (req,res,next)=>{
+//     console.log("third Handler");
+//     next(); // if next Handler is available then go to next handler otherwise go to the next line ;
+//     res.send("second Handler is here!!");
+// }
+// )
 
 
 app.listen(4000,()=>{
