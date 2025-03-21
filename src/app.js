ed72b34940400783ac41c58348af533c011c5324
kapil <kapilkeer1506@gmail.com>
2025-03-21 17:52:48 +0530

    add line

--- src/app.js
+++ src/app.js
@@ -9,12 +9,11 @@ app.use('/user',(req,res,next)=>{
 (req,res,next)=>{
     console.log("second Handler!!");
     next();
-    // res.send("second Handler is here!!"); //  getting error 
+    res.send("second Handler is here!!"); //  getting error 
 },
 (req,res,next)=>{
     console.log("third Handler");
     next(); // if next Handler is available then go to next handler otherwise go to the next line ;
-    res.send("third Handler is here .");
 }
 )
 
