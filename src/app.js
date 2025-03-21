6dd9698512d3bdb13d258f2aae1e14727b21cecb
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 16:12:45 +0530

    Play with next function in Routung

--- src/app.js
+++ src/app.js
@@ -1,29 +1,22 @@
 import express from "express";
 const app=express();
 
-// app.get('/user/:name/:id',(req,res)=>{
-//     console.log(req.params);
-//     res.send("getting data successfully!");
-// })
 app.use('/user',(req,res,next)=>{
-        console.log("1 handler!");
-        
-        next();
-   
-        // res.send("Rout 1");
+    console.log("first Handler!!");
+    next();
+
 },
 (req,res,next)=>{
-        console.log("2 handler!");
-        next(); // if available otherwise go to next line and there is not handler function here ,So ..
-        // res.send("Rout 2");
-       
+    console.log("second Handler!!");
+    next();
+    // res.send("second Handler is here!!"); //  getting error 
 },
 (req,res,next)=>{
-    console.log("3 Handler!!");
-    res.send("kapil is here!!!");
+    console.log("third Handler");
+    next(); // if next Handler is available then go to next handler otherwise go to the next line ;
+    res.send("third Handler is here .");
 }
 )
-
 
 
 app.listen(4000,()=>{
