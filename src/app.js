4d40a8eda4cc742180507bf88c03ef0f97252403
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 12:30:52 +0530

    exploring HTTP REQUEST METHOD LIKE (GET,POST,PUT,etc)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 const app=express();
-app.use('/Home',(req,res)=>{
-    res.send("kapil kumar is here and u also know me ");
+// app.use('/user',(req,res)=>{
+//     res.send("test is runing...");
+// })
+app.get('/user',(req,res)=>{
+    res.send("getting data successfully!");
 })
-app.use('/test',(req,res)=>{
-    res.send("test is runing...");
+app.post('/user',(req,res)=>{
+    // save the data in data base successfully;
+
+    res.send("data save succesfully!");
+});
+app.delete('/user',(req,res)=>{
+    res.send("delete the data successfully!");
 })
+
 
 app.listen(4000,()=>{
     console.log("Server is runing sucessful......");
