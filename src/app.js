dc3b91f0c4e23aea434b3849e14f842c1bd303bd
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 14:53:03 +0530

    learn about Routes param

--- src/app.js
+++ src/app.js
@@ -3,17 +3,18 @@ const app=express();
 // app.use('/user',(req,res)=>{
 //     res.send("test is runing...");
 // })
-app.get('/user',(req,res)=>{
+app.get('/user/:name/:id',(req,res)=>{
+    console.log(req.params);
     res.send("getting data successfully!");
 })
-app.post('/user',(req,res)=>{
-    // save the data in data base successfully;
+// app.post('/user',(req,res)=>{
+//     // save the data in data base successfully;
 
-    res.send("data save succesfully!");
-});
-app.delete('/user',(req,res)=>{
-    res.send("delete the data successfully!");
-})
+//     res.send("data save succesfully!");
+// });
+// app.delete('/user',(req,res)=>{
+//     res.send("delete the data successfully!");
+// })
 
 
 app.listen(4000,()=>{
