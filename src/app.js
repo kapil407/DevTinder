bb5c59f00854ab0163f608e3d84e7c10950c3fb8
kapil kumar <kapilkeer1998@gmail.com>
2025-03-21 16:01:57 +0530

    Play with next function in Routung

--- src/app.js
+++ src/app.js
@@ -1,20 +1,29 @@
 import express from "express";
 const app=express();
-// app.use('/user',(req,res)=>{
-//     res.send("test is runing...");
-// })
-app.get('/user/:name/:id',(req,res)=>{
-    console.log(req.params);
-    res.send("getting data successfully!");
-})
-// app.post('/user',(req,res)=>{
-//     // save the data in data base successfully;
 
-//     res.send("data save succesfully!");
-// });
-// app.delete('/user',(req,res)=>{
-//     res.send("delete the data successfully!");
+// app.get('/user/:name/:id',(req,res)=>{
+//     console.log(req.params);
+//     res.send("getting data successfully!");
 // })
+app.use('/user',(req,res,next)=>{
+        console.log("1 handler!");
+        
+        next();
+   
+        // res.send("Rout 1");
+},
+(req,res,next)=>{
+        console.log("2 handler!");
+        next(); // if available otherwise go to next line and there is not handler function here ,So ..
+        // res.send("Rout 2");
+       
+},
+(req,res,next)=>{
+    console.log("3 Handler!!");
+    res.send("kapil is here!!!");
+}
+)
+
 
 
 app.listen(4000,()=>{
