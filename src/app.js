35b1aad9d3ac6096fd0acc507185ef3ecf105e7a
kapil keer <kapilkeer1506@gmail.com>
2025-03-22 23:32:14 +0530

    fetch the data by emai ID and fetch all data of all users from data base

--- src/app.js
+++ src/app.js
@@ -7,25 +7,57 @@ const app=express();
 
 app.use(express.json());
 
-app.post("/signup",async (req,res)=>{
-    const User=new user(req.body);  
-    console.log(req.body);                         /*
-                                                    {
-                                                        firstName:"kapil",
-                                                        lastName:"kumar",
-                                                        emailId:"kapil@gmail.com",
-                                                        passward:"japil1235"
-                                                    }
-                                                      */
+// app.post("/signup",async (req,res)=>{
+//     const User=new user(req.body);  
+//     console.log(req.body);                         /*
+//                                                     {
+//                                                         firstName:"kapil",
+//                                                         lastName:"kumar",
+//                                                         emailId:"kapil@gmail.com",
+//                                                         passward:"japil1235"
+//                                                     }
+//                                                       */
+//     try{
+//    await User.save();
+//     res.send("data is save successfully");
+//     }
+//     catch(err){
+//         res.status(400).send("Error is "+ err.message);
+//     }
+// });
+
+// app.get('/user',async (req,res)=>{    // fetch data by emailId 
+//         const userEmail=req.body.emailId;
+//         try{
+//         const User=await user.find({emailId:userEmail});
+//         if(User.length===0){
+//             res.status(404).send("user not found");
+//         }    
+//         else{
+//               res.send(User);
+//         } 
+      
+//         }
+//         catch(err){
+//             res.status(404).send("somtthing went wrong");
+//         }
+// });
+
+// fetch all user from data base
+app.use('/user',async (req,res)=>{
+
     try{
-   await User.save();
-    res.send("data is save successfully");
+       const users=await user.find({});   // fetch all users's data from dataBase
+       if(!users.length){
+        res.status(404).send("users not found");
+
+       }
+       else res.send(users);
     }
     catch(err){
-        res.status(400).send("Error is "+ err.message);
+        res.status(404).send("something went wrong ");
     }
-});
-
+})
  
  
 
