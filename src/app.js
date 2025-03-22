a0c87129a422421ae6be17bcff01ffa1105ff1d9
kapil keer <kapilkeer1506@gmail.com>
2025-03-23 00:06:48 +0530

    fetch the user by its Id

--- src/app.js
+++ src/app.js
@@ -10,13 +10,12 @@ app.use(express.json());
 // app.post("/signup",async (req,res)=>{
 //     const User=new user(req.body);  
 //     console.log(req.body);                         /*
-//                                                     {
+//                                                      {
 //                                                         firstName:"kapil",
 //                                                         lastName:"kumar",
-//                                                         emailId:"kapil@gmail.com",
-//                                                         passward:"japil1235"
-//                                                     }
-//                                                       */
+//                                                       emailId:"kapil@gmail.com",
+//                                                        passward:"japil1235"                                         }
+//                                                        */
 //     try{
 //    await User.save();
 //     res.send("data is save successfully");
@@ -26,38 +25,38 @@ app.use(express.json());
 //     }
 // });
 
-// app.get('/user',async (req,res)=>{    // fetch data by emailId 
-//         const userEmail=req.body.emailId;
-//         try{
-//         const User=await user.find({emailId:userEmail});
-//         if(User.length===0){
-//             res.status(404).send("user not found");
-//         }    
-//         else{
-//               res.send(User);
-//         } 
+app.get('/user',async (req,res)=>{    // fetch data by emailId 
+        const userId=req.body._id;
+        try{
+        const User=await user.findById({_id:userId});  // it return an array of object
+        if(User.length===0){
+            res.status(404).send("user not found");
+        }    
+        else{
+              res.send(User);
+        } 
       
-//         }
-//         catch(err){
-//             res.status(404).send("somtthing went wrong");
-//         }
-// });
+        }
+        catch(err){
+            res.status(404).send("somtthing went wrong");
+        }
+});
 
 // fetch all user from data base
-app.use('/user',async (req,res)=>{
+// app.use('/user',async (req,res)=>{
 
-    try{
-       const users=await user.find({});   // fetch all users's data from dataBase
-       if(!users.length){
-        res.status(404).send("users not found");
+//     try{
+//        const users=await user.find({});   // fetch all users's data from dataBase
+//        if(!users.length){
+//         res.status(404).send("users not found");
 
-       }
-       else res.send(users);
-    }
-    catch(err){
-        res.status(404).send("something went wrong ");
-    }
-})
+//        }
+//        else res.send(users);
+//     }
+//     catch(err){
+//         res.status(404).send("something went wrong ");
+//     }
+// })
  
  
 
