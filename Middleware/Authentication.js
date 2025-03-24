6b80206527d87b334c5155161932a462e4b77b20
kapil keer <kapilkeer1506@gmail.com>
2025-03-24 23:18:06 +0530

    make Athentication by using token , cookies and jwt

--- Middleware/Authentication.js
+++ Middleware/Authentication.js
@@ -1,17 +1,27 @@
  // authentication
- export const auth=(req,res,next)=>{
-    const token='abc';
-    const authentication=token==='abc';
-    if(!authentication){
-        res.status(401).send("unauthorized admin");
+
+ import jwt from "jsonwebtoken";
+ import cookies from "cookie-parser";
+ import { user } from "../src/Models/User.js";
+
+export const UserAuth=async (req,res,next)=>{
+   try{
+    const cookies =req.cookies;    // extract the cookies
+    const {token}=cookies;  // extract the token from cookies
+    if(!token){
+        throw new Error("token is not valid ");
     }
-    else next();
-}
-export const UserAuth=(req,res,next)=>{
-            const token="xyz";
-            const authenticate=token==='xyz';
-            if(!authenticate){
-                res.status.send(401);
-            }
-            else next();
+    const decodedObj=await jwt.verify(token,"hdfsaidf#@$jd"); // vaditation the token or verify the token
+
+   const {_id}=decodedObj;                 
+    const User=await user.findById(_id); // find the user 
+    if(!User){
+        throw new Error("user not found");
+    }
+    req.User=User;   // add the User in request 
+    next();   // pass the control to request handler 
+   }
+   catch(err){
+        res.status(400).send("ERROR "+err.message);
+   }
 }
